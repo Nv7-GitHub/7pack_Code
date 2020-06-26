0e30b27a78bf75c6dcd372c486a0ae1920b6e09e
Nv7 <junk4Nv7@gmail.com>
2020-06-25 18:03:01 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from sys import executable
+from os import system
+from setuptools import setup
+from os import remove
+
+# put shebang
+lines = open("7pack.py").read().split("\n")
+lines.insert(0, "#!" + executable)
+lines = "\n".join(lines)
+with open("7pack", "w+") as f:
+    f.write(lines)
+    system("chmod +x " + "7pack")
+
+# Run Setup
+setup(name="7pack",
+      install_requires=["certifi", "chardet", "gitdb", "GitPython", "idna", "requests", "smmap", "urllib3"],
+      scripts=["7pack", "indexed.json"]
+      )
+remove("7pack")
